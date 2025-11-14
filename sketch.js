0848d512cbc936bc257f1f7ee5e157dd5ecc5538
letizianeri <letizia.neri@mail.polimi.it>
2025-11-14 15:55:06 +0100

    inizio impostazione pagina

--- sketch.js
+++ sketch.js
@@ -1,7 +1,78 @@
-function setup() {
-  createCanvas(400, 400);
+let data;
+
+let titleText = "STOLEN VOICES";
+let titleIndex = 0;
+
+
+let introText =
+  "This platform provides an interactive view of a dataset documenting journalists who have lost their lives in the pursuit of truth. Here, you can explore detailed data and personal stories that reveal the human and historical impact of these events.\n\nUse the right arrow key on your keyboard to navigate through the sections and discover the data in an intuitive, interactive way.";
+
+let introIndex = 0;
+let introStarted = false; //evita doppia animazione
+
+
+function preload() {
+  data = loadTable("assets/data.csv", "csv", "header");
 }
 
+
+function setup() {
+  let pageHeight = document.documentElement.scrollHeight;
+  createCanvas(windowWidth, pageHeight);
+  
+  typeTitle();
+  setupIntroTrigger();
+}
+
+//animazione che scrive il titolo
+function typeTitle() {
+  let titleEl = document.getElementById("title");
+
+  if (titleIndex < titleText.length) {
+    titleEl.textContent += titleText.charAt(titleIndex);
+    titleIndex++;
+    setTimeout(typeTitle, 200);
+  }
+}
+
+//l animazione intro text appare solo quando il testo si trova al centro della pagina
+function setupIntroTrigger() {
+  let target = document.getElementById("intro-text");
+
+  let observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting && !introStarted) {
+        introStarted = true;
+        typeIntroText();
+      }
+    });
+  }, {
+    root: null,
+    threshold: 0.6     //60% dell'epigrafe visibile
+  });
+
+  observer.observe(target);
+}
+
+//animazione intro text
+function typeIntroText() {
+  let introEl = document.getElementById("intro-text");
+  introEl.style.opacity = 1;
+
+  if (introIndex < introText.length) {
+    introEl.textContent += introText.charAt(introIndex);
+    introIndex++;
+    setTimeout(typeIntroText, 70);
+  }
+}
+
+
 function draw() {
-  background(220);
+  background(25);
+}
+
+
+function windowResized() {
+  let pageHeight = document.documentElement.scrollHeight;
+  resizeCanvas(windowWidth, pageHeight);
 }
