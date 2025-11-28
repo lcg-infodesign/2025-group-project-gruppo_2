ae69f79c5fee4e8728b83acf455f430b854b3539
filippogarbero <garbero.filippo@gmail.com>
2025-11-28 10:11:28 +0100

    modifiche sara

--- main.js
+++ main.js
@@ -141,7 +141,7 @@ function setup() {
   padding = 30;
 
   let canvas = createCanvas(mainWidth, windowHeight);
-  canvas.position(0, 80);
+  canvas.position(0, 40);
 
 
   yLabelWidth = padding + 60;
